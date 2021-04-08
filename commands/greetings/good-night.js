06b4823693b56b4a8ecf7c8b4fcee1427b25f121
Diego <me@diegor.nl>
2021-04-08 14:00:33 +0200

    Create good-night greeting command

---
+++ commands/greetings/good-night.js
@@ -0,0 +1,17 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class MeowCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'good-night',
+			aliases: ['gn', 'night'],
+			group: 'greetings',
+			memberName: 'good night',
+			description: 'Replies with a good night greeting.',
+		});
+	}
+
+	run(message) {
+		return message.say('Sweet dreams, ' + message.author.toString() + '!', { files: ['https://media1.tenor.com/images/3c15ee9f4b4277389d0573e8ded3f015/tenor.gif'] });
+	}
+};
