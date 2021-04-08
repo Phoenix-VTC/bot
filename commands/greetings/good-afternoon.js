68c0f12b700c7b6982e5604192400e6940496659
Diego <me@diegor.nl>
2021-04-08 13:57:11 +0200

    Create good-afternoon greeting command

---
+++ commands/greetings/good-afternoon.js
@@ -0,0 +1,17 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class MeowCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'good-afternoon',
+			aliases: ['ga', 'afternoon'],
+			group: 'greetings',
+			memberName: 'good afternoon',
+			description: 'Replies with a good afternoon greeting.',
+		});
+	}
+
+	run(message) {
+		return message.say('Enjoy the rest of your day, ' + message.author.toString() + '!', { files: ['https://media1.tenor.com/images/13c17157b40df6b90254e5be810caeeb/tenor.gif'] });
+	}
+};
