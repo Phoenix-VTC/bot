6a66c72febf6001c6dfbf50e6fdf58e721535cd2
Diego <me@diegor.nl>
2021-09-13 14:23:21 +0200

    Improve ping command

--- commands/utils/ping.js
+++ commands/utils/ping.js
@@ -3,6 +3,8 @@ module.exports = {
 	description: 'Ping!',
 	cooldown: 5,
 	execute(message) {
-		message.channel.send('Pong.');
+		message.channel.send('Loading data').then(async (msg) => {
+			msg.edit(`🏓 Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(message.client.ws.ping)}ms`);
+		});
 	},
 };
