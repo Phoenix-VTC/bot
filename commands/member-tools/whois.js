f097e47d8c755b4e2037b5ac15c6c97d06bc05ab
Diego <me@diegor.nl>
2021-09-13 16:01:36 +0200

    Fix whois command 404 response

--- commands/member-tools/whois.js
+++ commands/member-tools/whois.js
@@ -26,11 +26,12 @@ module.exports = {
 		}
 
 		const response = await fetch(`${base_auth_url}/api/discord-bot/users/${id}`, { headers: { 'token': base_auth_token } });
-		const userData = await response.json();
 
-		if (userData['status_code'] === 404) {
+		if (response.status === 404) {
 			return message.channel.send(`We couldn't find this user on the PhoenixBase, ${message.author}!`);
 		}
+
+		const userData = await response.json();
 
 		if (userData['message']) {
 			return message.channel.send(`Well, this is awkward... Something went wrong while trying to run this command. \n**Error message:** \`${userData['message']}\``);
